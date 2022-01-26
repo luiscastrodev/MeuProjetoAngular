478ed6acdeea237ff6ff16223fc84b377ed1e738
Luis Ricardo <luisricardodecastro@gmail.com>
2022-01-26 01:15:05 -0300

    Event Binding

--- src/app/demos/data-bing/data-bing.component.ts
+++ src/app/demos/data-bing/data-bing.component.ts
@@ -12,7 +12,14 @@ export class DataBingComponent implements OnInit {
   ngOnInit(): void {
   }
 
- public contadorClique: number = 1;
+ public contadorClique: number = 0;
  public minhaImagem: String = "https://angular.io/assets/images/logos/angular/logo-nav@2x.png";
 
+ adicionarCliqueMais(){
+  this.contadorClique++;
+ }
+
+ adicionarCliqueMenos(){
+  this.contadorClique--;
+ }
 }
